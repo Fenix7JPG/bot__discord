a9b6403f63e56d6659f33db9de132db7c020da5a
Fenix7JPG <fenix7jpg@gmail.com>
2025-09-17 18:51:57 -0500

    Update ia.py

--- ia.py
+++ ia.py
@@ -1,7 +1,7 @@
 
 import cohere
 from collections import deque
-
+import random
 
 class IA():
     def __init__(self,api):
@@ -36,24 +36,27 @@ class IA():
         historial.extend(self.historial)
         self.historial.append({"role": "USER", "message": message})
 
-        response = self.co.chat(
-            model="command-a-03-2025",
-            message= "Analiza la conversación. Si crees que aporta valor responder, devuelve 'SI'. Si no es necesario participar, devuelve 'NO'. No escribas nada más.",
-            chat_history=historial, # pasamos todo menos el último, que ya está en "message"
-            max_tokens=2,
-        )
-        bot_reply = response.text.strip()
-        if bot_reply.upper() == "SI":
+        if random.randint(0,20) == 20:
+            
             response = self.co.chat(
                 model="command-a-03-2025",
-                message=message,
+                message= "Analiza la conversación. Si crees que aporta valor responder, devuelve 'SI'. Si no es necesario participar, devuelve 'NO'. No escribas nada más.",
                 chat_history=historial, # pasamos todo menos el último, que ya está en "message"
-                
+                max_tokens=2,
             )
-            bot_reply = responde.text.strip()
-            self.historial.append({"role": "CHATBOT", "message": bot_reply})
-            return bot_reply
+            bot_reply = response.text.strip()
+            if bot_reply.upper() == "SI":
+                response = self.co.chat(
+                    model="command-a-03-2025",
+                    message=message,
+                    chat_history=historial, # pasamos todo menos el último, que ya está en "message"
+                    
+                )
+                bot_reply = responde.text.strip()
+                self.historial.append({"role": "CHATBOT", "message": bot_reply})
+                return bot_reply
         return None
+
 
 
 
