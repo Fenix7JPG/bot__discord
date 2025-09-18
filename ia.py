5716eb36c3d988509834bd9d82e9821fec698628
Fenix7JPG <fenix7jpg@gmail.com>
2025-09-17 20:04:37 -0500

    Update ia.py

--- ia.py
+++ ia.py
@@ -8,9 +8,7 @@ class IA():
         self.api_key = api
         self.co = cohere.Client(self.api_key)
         self.historial = deque(maxlen=29)
-        self.instruction = {"role": "USER", "message": "Instrucción: Eres un manager tímido y torpe pero competente. Tu tono es humilde, educado y ligeramente inseguro, pero siempre profesional."}
-    def set_instruction(self, instruction: str):
-        self.instruction = {"role": "USER", "message": f"Instrucción: {instruction}"}
+        self.instruction = "Instrucción: Eres un manager tímido y torpe pero competente. Tu tono es humilde, educado y ligeramente inseguro, pero siempre profesional."
 
     def chat(self,message):
         historial = []
@@ -23,7 +21,7 @@ class IA():
             model="command-a-03-2025",
             message=message,
             chat_history=historial, # pasamos todo menos el último, que ya está en "message"
-            preamble=self.instruction["message"]
+            preamble=self.instruction
             
         )
         bot_reply = response.text.strip()
@@ -43,6 +41,7 @@ class IA():
                 model="command-a-03-2025",
                 message= "Analiza la conversación. Si crees que aporta valor responder, devuelve 'SI'. Si no es necesario participar, devuelve 'NO'. No escribas nada más.",
                 chat_history=historial, # pasamos todo menos el último, que ya está en "message"
+                preamble=self.instruction
                 max_tokens=2,
             )
             bot_reply = response.text.strip()
@@ -50,6 +49,7 @@ class IA():
                 response = self.co.chat(
                     model="command-a-03-2025",
                     message=message,
+                    preamble=self.instruction
                     chat_history=historial, # pasamos todo menos el último, que ya está en "message"
                     
                 )
@@ -57,6 +57,7 @@ class IA():
                 self.historial.append({"role": "CHATBOT", "message": bot_reply})
                 return bot_reply
         return None
+
 
 
 
